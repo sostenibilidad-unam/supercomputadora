4902f618ad4bf20ebb396bcdb0ec0dbfdccaf984
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2019-02-26 12:03:44 -0600

    condor config generator for hostname

--- condor_node/condor_setup.py
+++ condor_node/condor_setup.py
@@ -5,11 +5,19 @@ parser.add_argument('hostname')
 args = parser.parse_args()
 
 config_d = """
+
 CONDOR_HOST = patung.lancis.ecologia.unam.mx
+UID_DOMAIN = lancis.ecologia.unam.mx
+TRUST_UID_DOMAIN = TRUE
+FILESYSTEM_DOMAIN = wuyafs
+USE_NFS = True
 
 BIND_ALL_INTERFACES = TRUE
-DAEMON_LIST = STARTD, SCHEDD, COLLECTOR, NEGOTIATOR, MASTER
-ALLOW_WRITE = patung patung.lancis.ecologia.unam.mx 127.0.0.1 localhost *.lancis.ecologia.unam.mx 192.168.0.* {hostname} {hostname}.local
+DAEMON_LIST = STARTD, MASTER
+
+ALLOW_WRITE = patung patung.lancis.ecologia.unam.mx 127.0.0.1 localhost *.lancis.ecologia.unam.mx 192.168.0.* {hostname}.local {hostname}
+
+ALLOW_NEGOTIATOR = patung patung.lancis.ecologia.unam.mx 127.0.0.1 localhost *.lancis.ecologia.unam.mx 192.168.0.* {hostname}.local {hostname}
 
 START = TRUE
 SUSPEND = FALSE
@@ -17,18 +25,11 @@ CONTINUE = TRUE
 PREEMPT = FALSE
 KILL = FALSE
 
-
-UID_DOMAIN = lancis.ecologia.unam.mx
-TRUST_UID_DOMAIN = TRUE
-FILESYSTEM_DOMAIN = wuyafs
-USE_NFS = True
-
-#JAVA=/proyectos/opt/jre1.8.0_73/bin/java
-
 NUM_SLOTS = 1
 NUM_SLOTS_TYPE_1 = 1
 SLOT_TYPE_1 = cpus=100%
 SLOT_TYPE_1_PARTITIONABLE = true
+
 """
 
 print config_d.format(hostname=args.hostname)
